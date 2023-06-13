fb5561e73c92482fd6060d29e748b533212b8f16
Carmen Birkle <carmen.birkle@iu-study.org>
2023-06-13 08:42:03 +0200

    setup navbar and header desktop and responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
+import { MatSidenavContainer } from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'simple-crm';
+export class AppComponent implements AfterViewInit {
+  @ViewChild(MatSidenavContainer) sidenavContainer!: MatSidenavContainer;
+
+  @ViewChild('drawer') drawer!: MatDrawer;
+  isMobileView = false;
+
+  @HostListener('window:resize', ['$event'])
+  onResize($event: any) {
+    this.checkViewport();
+  }
+
+  ngAfterViewInit() {
+    this.checkViewport();
+    this.sidenavContainer.scrollable.elementScrolled().subscribe(() => {
+      // React to scrolling
+    });
+  }
+
+  checkViewport() {
+    this.isMobileView = window.innerWidth <= 768; // TODO Adjust the breakpoint
+
+    // Set the attributes based on viewport
+    this.drawer.opened = !this.isMobileView;
+    this.drawer.mode = this.isMobileView ? 'over' : 'side';
+  }
 }
